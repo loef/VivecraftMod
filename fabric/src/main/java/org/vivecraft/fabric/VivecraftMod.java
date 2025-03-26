beaa584a47daa017ef88934e6835ed0334d947b1
fayer3 <bayer.florian@aon.at>
2025-03-26 15:03:41 +0100

    fix fabric server crash, add neoforge

--- fabric/src/main/java/org/vivecraft/fabric/VivecraftMod.java
+++ fabric/src/main/java/org/vivecraft/fabric/VivecraftMod.java
@@ -2,15 +2,11 @@ package org.vivecraft.fabric;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.Minecraft;
-import org.vivecraft.client.gui.settings.VivecraftMainSettings;
 import org.vivecraft.client.network.ClientNetworking;
 import org.vivecraft.common.network.packet.c2s.VivecraftPayloadC2S;
 import org.vivecraft.common.network.packet.s2c.VivecraftPayloadS2C;
@@ -37,13 +33,7 @@ public class VivecraftMod implements ModInitializer {
         if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
             ClientPlayNetworking.registerGlobalReceiver(VivecraftPayloadS2C.TYPE,
                 (payload, context) -> ClientNetworking.handlePacket(payload));
-
-            ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(
-                ClientCommandManager.literal("vivecraft-client-config").executes(context -> {
-                    Minecraft mc = context.getSource().getClient();
-                    mc.schedule(() -> mc.setScreen(new VivecraftMainSettings(mc.screen)));
-                    return 1;
-                })));
+            ClientEvents.registerClientCommands();
         }
 
         ServerPlayNetworking.registerGlobalReceiver(VivecraftPayloadC2S.TYPE,
