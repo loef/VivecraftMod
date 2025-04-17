loef/VivecraftMod

commit 23304191a2fd1ae39c726c11e0d2a926d9fccd06

add a separate VRDevicePose for the center view, that has the smoothed position and rotation, and use the camera position/rotation when getting the eye position instead of recalculating it every time, also set the camera rotation to the full rotation, not just pitch and yaw

common/src/main/java/org/vivecraft/client_vr/VRData.java
@@ -18,6 +18,9 @@ import java.lang.Math;
 public class VRData {
     // headset center
     public VRDevicePose hmd;
+    // smoothed headset center
+    public VRDevicePose center;
+
     // left eye
     public VRDevicePose eye0;
     // right eye
@@ -75,6 +78,23 @@ public class VRData {
 
         // headset
         this.hmd = new VRDevicePose(this, mcVR.hmdRotation, scaledPos, mcVR.getHmdVector());
+
+        // smoothed headset, only when needed
+        float smoothing = dataHolder.vrSettings.displayMirrorCenterSmooth;
+        if (smoothing > 0.0F && (dataHolder.vrSettings.displayMirrorMode == VRSettings.MirrorMode.FIRST_PERSON ||
+            (dataHolder.vrSettings.displayMirrorMode == VRSettings.MirrorMode.MIXED_REALITY &&
+                dataHolder.vrSettings.mixedRealityUndistorted
+            )
+        ))
+        {
+            Matrix4f smoothedRotation = new Matrix4f().rotation(mcVR.hmdRotHistory.averageRotation(smoothing)).transpose();
+            this.center = new VRDevicePose(this,
+                smoothedRotation,
+                mcVR.hmdHistory.averagePosition(dataHolder.vrSettings.displayMirrorCenterSmooth).add(scaleOffset),
+                smoothedRotation.transformDirection(MathUtils.BACK, new Vector3f()));
+        } else {
+            this.center = this.hmd;
+        }
 
         this.eye0 = new VRDevicePose(this,
             mcVR.getEyeRotation(RenderPass.LEFT),
@@ -357,7 +377,7 @@ public class VRData {
      */
     public VRDevicePose getEye(RenderPass pass) {
         return switch (pass) {
-            case CENTER -> this.hmd;
+            case CENTER -> this.center;
             case LEFT -> this.eye0;
             case RIGHT -> this.eye1;
             case THIRD -> this.c2;

common/src/main/java/org/vivecraft/client_vr/QuaternionfHistory.java
@@ -44,7 +44,7 @@ public class QuaternionfHistory {
      * @return the newest Quaternion
      */
     public Quaternionf latest() {
-        return this.data.getLast().quat;
+        return this.data.isEmpty() ? new Quaternionf() : this.data.getLast().quat;
     }
 
     /**

common/src/main/java/org/vivecraft/client_vr/gameplay/screenhandlers/GuiHandler.java
@@ -673,7 +673,7 @@ public class GuiHandler {
             GUI_SCALE = 1.0F;
         }
 
-        Vec3 eye = RenderHelper.getSmoothCameraPosition(currentPass, DH.vrPlayer.vrdata_world_render);
+        Vec3 eye = MC.gameRenderer.getMainCamera().getPosition();
 
         Vec3 translation = guipos.subtract(eye);
         poseMatrix.translate((float) translation.x, (float) translation.y, (float) translation.z);

common/src/main/java/org/vivecraft/client_vr/gameplay/trackers/CameraTracker.java
@@ -68,7 +68,7 @@ public class CameraTracker extends Tracker {
         }
 
         // chunk renderer gets angry if we're really far away, force hide when >3/4 render distance
-        if (this.dh.vrPlayer.vrdata_world_render.getEye(RenderPass.CENTER).getPosition().distanceTo(this.position) >
+        if (this.dh.vrPlayer.vrdata_world_render.hmd.getPosition().distanceTo(this.position) >
             this.mc.options.getEffectiveRenderDistance() * 12)
         {
             this.visible = false;

common/src/main/java/org/vivecraft/client_vr/render/XRCamera.java
@@ -6,6 +6,7 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.material.FogType;
+import org.joml.Matrix4f;
 import org.joml.Vector3f;
 import org.vivecraft.client_vr.ClientDataHolderVR;
 import org.vivecraft.client_vr.VRData;
@@ -41,23 +42,16 @@ public class XRCamera extends Camera {
         RenderPass renderpass = dataholder.currentPass;
 
         VRData.VRDevicePose eye = dataholder.vrPlayer.getVRDataWorld().getEye(renderpass);
-        if (renderpass == RenderPass.CENTER && dataholder.vrSettings.displayMirrorCenterSmooth > 0.0F) {
-            this.setPosition(RenderHelper.getSmoothCameraPosition(renderpass, dataholder.vrPlayer.getVRDataWorld()));
-        } else {
-            this.setPosition(eye.getPosition());
-        }
+        this.setPosition(eye.getPosition());
         this.xRot = -eye.getPitch();
         this.yRot = eye.getYaw();
         this.getLookVector().set(eye.getDirection());
-        Vector3f up = eye.getCustomVector(MathUtils.UP);
-        this.getUpVector().set(up);
-        Vector3f left = eye.getCustomVector(MathUtils.RIGHT);
-        this.getLeftVector().set(left);
 
-        this.rotation().rotationYXZ(
-            Mth.PI - this.yRot * Mth.DEG_TO_RAD,
-            -this.xRot * Mth.DEG_TO_RAD,
-            0.0F);
+        Matrix4f rotation = eye.getMatrix();
+        rotation.transformDirection(MathUtils.UP, this.getUpVector());
+        rotation.transformDirection(MathUtils.RIGHT, this.getLeftVector());
+
+        this.rotation().setFromNormalized(rotation);
     }
 
     /**

common/src/main/java/org/vivecraft/client_vr/render/helpers/DebugRenderHelper.java
@@ -70,8 +70,7 @@ public class DebugRenderHelper {
     public static void renderPlayerAxes(float partialTick) {
         if (MC.player != null) {
             BufferBuilder bufferbuilder = null;
-            Vec3 camPos = RenderHelper
-                .getSmoothCameraPosition(DATA_HOLDER.currentPass, DATA_HOLDER.vrPlayer.getVRDataWorld());
+            Vec3 camPos = MC.gameRenderer.getMainCamera().getPosition();
 
             for (Player p : MC.player.level().players()) {
                 if (ClientVRPlayers.getInstance().isVRPlayer(p)) {
@@ -179,7 +178,7 @@ public class DebugRenderHelper {
      */
     private static void renderTackerPositions(boolean showNames) {
         VRData data = DATA_HOLDER.vrPlayer.getVRDataWorld();
-        Vec3 camPos = RenderHelper.getSmoothCameraPosition(DATA_HOLDER.currentPass, data);
+        Vec3 camPos = MC.gameRenderer.getMainCamera().getPosition();
         Quaternionf orientation = data.getEye(DATA_HOLDER.currentPass).getMatrix()
             .getNormalizedRotation(new Quaternionf())
             .rotateY(Mth.PI);
@@ -252,8 +251,7 @@ public class DebugRenderHelper {
     private static void addAxes(
         BufferBuilder bufferBuilder, VRData data, VRData.VRDevicePose pose)
     {
-        Vector3f position = pose.getPosition()
-            .subtract(RenderHelper.getSmoothCameraPosition(DATA_HOLDER.currentPass, data)).toVector3f();
+        Vector3f position = pose.getPosition().subtract(MC.gameRenderer.getMainCamera().getPosition()).toVector3f();
 
         float scale = 0.25F * DATA_HOLDER.vrPlayer.worldScale;
 
@@ -365,7 +363,7 @@ public class DebugRenderHelper {
      */
     public static void renderTextAtPosition(PoseStack poseStack, Vec3 position, String text) {
         VRData data = DATA_HOLDER.vrPlayer.getVRDataWorld();
-        Vec3 camPos = RenderHelper.getSmoothCameraPosition(DATA_HOLDER.currentPass, data);
+        Vec3 camPos = MC.gameRenderer.getMainCamera().getPosition();
         Quaternionf rot = data.getEye(DATA_HOLDER.currentPass).getMatrix()
             .getNormalizedRotation(new Quaternionf())
             .rotateY(Mth.PI);

common/src/main/java/org/vivecraft/client_vr/render/helpers/RenderHelper.java
@@ -59,12 +59,7 @@ public class RenderHelper {
      * @param renderPass RenderPass to get the rotation matrix for
      */
     public static Matrix4f getVRModelView(RenderPass renderPass) {
-        if (renderPass == RenderPass.CENTER && DATA_HOLDER.vrSettings.displayMirrorCenterSmooth > 0.0F) {
-            return new Matrix4f().rotation(MCVR.get().hmdRotHistory
-                .averageRotation(DATA_HOLDER.vrSettings.displayMirrorCenterSmooth));
-        } else {
-            return DATA_HOLDER.vrPlayer.vrdata_world_render.getEye(renderPass).getMatrix().transpose();
-        }
+        return DATA_HOLDER.vrPlayer.vrdata_world_render.getEye(renderPass).getMatrix().transpose();
     }
 
     /**
@@ -91,25 +86,6 @@ public class RenderHelper {
     }
 
     /**
-     * Gets the camera position of the given RenderPass.
-     * If the RenderPass is CENTER the position is smoothed over time if that setting is on
-     *
-     * @param renderPass pass to get the camera position for
-     * @param vrData     vrData to get it from
-     * @return camera position
-     */
-    public static Vec3 getSmoothCameraPosition(RenderPass renderPass, VRData vrData) {
-        if (DATA_HOLDER.currentPass == RenderPass.CENTER && DATA_HOLDER.vrSettings.displayMirrorCenterSmooth > 0.0F) {
-            Vector3f pos = MCVR.get().hmdHistory.averagePosition(DATA_HOLDER.vrSettings.displayMirrorCenterSmooth)
-                .mul(vrData.worldScale)
-                .rotateY(vrData.rotation_radians);
-            return new Vec3(pos.x + vrData.origin.x, pos.y + vrData.origin.y, pos.z + vrData.origin.z);
-        } else {
-            return vrData.getEye(renderPass).getPosition();
-        }
-    }
-
-    /**
      * Applies the offset for the LEFT and RIGHT RenderPass from the headset position
      * Other RenderPasses do nothing
      *
@@ -119,8 +95,7 @@ public class RenderHelper {
     public static void applyStereo(RenderPass renderPass, PoseStack poseStack) {
         if (renderPass == RenderPass.LEFT || renderPass == RenderPass.RIGHT) {
             Vec3 eye = DATA_HOLDER.vrPlayer.vrdata_world_render.getEye(renderPass).getPosition()
-                .subtract(DATA_HOLDER.vrPlayer.vrdata_world_render.getEye(RenderPass.CENTER)
-                    .getPosition());
+                .subtract(DATA_HOLDER.vrPlayer.vrdata_world_render.hmd.getPosition());
             poseStack.translate(-eye.x, -eye.y, -eye.z);
         }
     }
@@ -184,8 +159,7 @@ public class RenderHelper {
      */
     public static void setupRenderingAtController(int c, Matrix4f matrix) {
         Vec3 aimSource = getControllerRenderPos(c);
-        aimSource = aimSource.subtract(
-            getSmoothCameraPosition(DATA_HOLDER.currentPass, DATA_HOLDER.vrPlayer.getVRDataWorld()));
+        aimSource = aimSource.subtract(MC.gameRenderer.getMainCamera().getPosition());
         // move from head to hand origin.
         matrix.translate((float) aimSource.x, (float) aimSource.y, (float) aimSource.z);
 

common/src/main/java/org/vivecraft/client_vr/render/helpers/VRArmHelper.java
@@ -419,8 +419,7 @@ public class VRArmHelper {
 
             double segmentProgress = 1.0D / (double) segments;
 
-            Vec3 cameraPosition = RenderHelper.getSmoothCameraPosition(DATA_HOLDER.currentPass,
-                DATA_HOLDER.vrPlayer.getVRDataWorld());
+            Vec3 cameraPosition = MC.gameRenderer.getMainCamera().getPosition();
 
             // arc
             for (int i = 0; i < segments; i++) {

commit 72a09967e823a7d59ea72d9c4dc403ebaf78f19d

fix double rotation on smooth mirror caused by 23304191a2fd1ae39c726c11e0d2a926d9fccd06

common/src/main/java/org/vivecraft/client_vr/VRData.java
@@ -87,10 +87,11 @@ public class VRData {
             )
         ))
         {
-            Matrix4f smoothedRotation = new Matrix4f().rotation(mcVR.hmdRotHistory.averageRotation(smoothing)).transpose();
+            Matrix4f smoothedRotation = new Matrix4f().rotation(mcVR.hmdRotHistory.averageRotation(smoothing))
+                .rotateY(this.rotation_radians).transpose();
             this.center = new VRDevicePose(this,
                 smoothedRotation,
-                mcVR.hmdHistory.averagePosition(dataHolder.vrSettings.displayMirrorCenterSmooth).add(scaleOffset),
+                mcVR.hmdHistory.averagePosition(smoothing).add(scaleOffset),
                 smoothedRotation.transformDirection(MathUtils.BACK, new Vector3f()));
         } else {
             this.center = this.hmd;

common/src/main/java/org/vivecraft/client_vr/provider/MCVR.java
@@ -642,7 +642,7 @@ public abstract class MCVR {
 
         // conjugate, because camera matrices need to be transposed
         this.hmdRotHistory.add(new Quaternionf().setFromNormalized(this.hmdRotation).conjugate()
-            .rotateY((float) -Math.toRadians(this.dh.vrSettings.worldRotation)));
+            .rotateY((float) Math.toRadians(this.dh.vrSettings.worldRotation)));
 
 
         // controllers